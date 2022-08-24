ccc8c61fe224ef42ae6b296704369128228d8b93
NiKk0123 <nikko16lastristica@gmail.com>
2022-08-24 22:18:27 +0800

    src all coding nandito

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+  OnSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+  render() {
+    // console.log("render from AppJS");
+    const { monsters, searchField } = this.state;
+    const { OnSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <SearchBox
+          OnChangeHandler={OnSearchChange}
+          placeholder="Search Monsters"
+          className="search-box"
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
